dadd28f54d63f25d771547e1ca16c03d0b362949
Chauncy <cgwang1580@gmail.com>
2019-01-03 10:30:12 +0800

    first commit

---
+++ app/src/main/java/com/camera2glview/chauncy_wang/camera2glviewtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.camera2glview.chauncy_wang.camera2glviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
